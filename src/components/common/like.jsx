05855b6b15de2d476986ba5ad49285b0ea815775
Alexandra <55248127+randomartlover@users.noreply.github.com>
2020-06-27 15:43:59 +0100

    Pagination Added

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
 //Input: liked: boolean
 //Output: onClick
 
-const Like = props => {
-    let classes = "fa fa-heart"
-    if (!props.liked) classes+= "-o"
-    return ( <i onClick={props.onClick} style={{cursor:"pointer"}}className={classes} aria-hidden="true"></i> );
- ;
-}
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
 
 export default Like;
